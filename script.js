e9dab6acdf15821c3b3791aac7d54ebd7956911a
Andre Entrekin <andre.entrekin@gmail.com>
2019-12-22 17:30:23 -0600

    colors are working

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ $(document).ready(function () {
         var test = ($(element).attr("data-value"))
         var arr = test.split(', ')
         console.log(arr);
-        if (arr === d.getHours) {
+        if (arr == d.getHours) {
             this.classList.add("present")
 
         } else if (arr < d.getHours) {
