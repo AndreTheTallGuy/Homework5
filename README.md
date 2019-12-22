cd19202673a40a5b50b09bffc6d54a0d1b9752bc
Andre Entrekin <andre.entrekin@gmail.com>
2019-12-22 16:30:28 -0600

    as good as it's getting

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Homework5
+This website is fully functional Work Day Scheduler. It helps you keep track of your work day by color coding the day. The current hour will always be green with the past turning grey. Additionally, this site boasts an autosave feature so your events are protected even if you forget to save! 
+
--- script.js
+++ script.js
@@ -1,15 +1,121 @@
+$(document).ready(function () {
+
+    var d = new Date()
+    d.getHours()
+    console.log(d.getHours())
 
 
-var d = new Date()
-d.getHours()
-console.log(d.getHours())
+
+
+    var event = $(".description")
 
 
 
 
-var time = $(".description")
-for (let i = 0; i < time.length; i++) {
-    var timeArr = time[i];
-    var timeEl = JSON.stringify(timeArr)
-    console.log(timeEl.dataValue)
-}
+    // for (let i = 0; i < event.length; i++) {
+    //     var test = event[i];
+    //     console.log(this)
+
+
+    //     console.log(event[i]);
+
+
+    // var test = $(".description[data-value]")
+    // for (let i = 0; i < test.length; i++) {
+    //     const element = test[i];
+    //     console.log(element);
+
+
+
+
+    // var timeEl = JSON.stringify(timeArr)
+    // console.log(timeEl.value)
+    // console.log((timeArr).data('value'));
+    // }
+
+    // for (let i = 0; i < 9; i++) {
+    //     const element = event[i];
+
+    $.each($("*[data-value]"), function (index, element) {
+        var test = ($(element).attr("data-value"))
+        console.log(test);
+
+
+        //     var testParse = JSON.parse(test)
+        //     console.log(testParse);
+        //     if (testParce === 15) {
+        //         event.classList.add("present")
+        //     }
+
+
+
+
+        // for (let i = 0; i < test.length; i++) {
+        //         const element = test[i];
+        //         if (element === 15) {
+        //             event.classlist.add('present')
+
+        //             console.log(test);
+
+    })
+
+
+
+
+
+    // });
+
+    // }
+
+
+
+
+
+
+
+
+
+
+    $("*[data-value]").each(function () {
+        $(this).val(localStorage.getItem("item-" + $(this).attr("data-value")));
+    });
+
+
+    $("*[data-value]").on("keyup", function (itm) {
+        localStorage.setItem("item-" + $(this).attr("data-value"), $(this).val());
+    })
+    // var save = $(".saveBtn")
+
+    // $document.on("click", ".saveBtn", function () {
+    //     localStorage.setItem("item-" + $(this).attr("data-value"), $(this).val());
+
+    // })
+
+
+
+
+
+
+
+    // save.on("click", function () {
+    //     $(this).val(localStorage.setItem("event", event))
+    //     console.log(this);
+
+
+
+
+
+
+    // localStorage.setItem("event", event)
+
+    // console.log(event);
+    // var localStor = localStorage.getItem(event)
+    // var local;
+    // if (localStor === null) {
+    //     local = [];
+    // } else {
+    //     local = JSON.parse(localStor)
+    // }
+    // console.log(local);
+
+})
